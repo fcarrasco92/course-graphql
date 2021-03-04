897fbb7e13b3e95f3ac74ce3d1f25d6c7dacb503
Felipe Carrasco <felipe@bakers.dev>
2021-03-04 01:36:27 -0300

    feature: setting the DB, Query and arguments

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-"use strict";
+'use strict'
 
-const { buildSchema } = require("graphql");
-const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
-const { readFileSync } = require("fs");
-const { join } = require("path");
-const resolvers = require("./lib/resolvers");
+require('dotenv').config()
+const { makeExecutableSchema } = require('graphql-tools')
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
-const app = express();
-const port = process.env.port || 3000;
+const app = express()
+const port = process.env.port || 3000
 
 // define initial Schema
 /**
@@ -20,9 +21,12 @@ const port = process.env.port || 3000;
     - ID
  */
 
-const schema = buildSchema(
-  readFileSync(join(__dirname, "lib", "schema.graphql"), "utf-8")
-);
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
+)
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 // Execute the query hello in terminal
 // graphql(schema, "{ greeting }", resolvers).then((data) => {
@@ -30,14 +34,14 @@ const schema = buildSchema(
 // });
 
 app.use(
-  "/api",
+  '/api',
   graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true,
+    graphiql: true
   })
-);
+)
 
 app.listen(port, () => {
-  console.log(`server is listening at http://localhost:${port}`);
-});
+  console.log(`server is listening at http://localhost:${port}`)
+})
