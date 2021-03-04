8def1322e87503ba828bd7c1e3be3b9ec1bdb97d
Felipe Carrasco <felipe@bakers.dev>
2021-03-03 21:45:53 -0300

    feature: config structure folder and custom types define

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 "use strict";
 
-const { graphql, buildSchema } = require("graphql");
+const { buildSchema } = require("graphql");
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const resolvers = require("./lib/resolvers");
 
 const app = express();
 const port = process.env.port || 3000;
@@ -17,18 +20,9 @@ const port = process.env.port || 3000;
     - ID
  */
 
-const schema = buildSchema(`
-    type Query{
-        hello: String,       
-    }
-`);
-
-// setting the resolvers
-const resolvers = {
-  hello: () => {
-    return "Hello world";
-  },
-};
+const schema = buildSchema(
+  readFileSync(join(__dirname, "lib", "schema.graphql"), "utf-8")
+);
 
 // Execute the query hello in terminal
 // graphql(schema, "{ greeting }", resolvers).then((data) => {
